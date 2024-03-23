e1b184cce044e8c8fbc04af4a1fb907e61fff4b5
Insane96 <delvillano.alberto@gmail.com>
2024-03-23 16:38:51 +0100

    Fixed m.a. jump effect not playing server side

--- src/main/java/insane96mcp/iguanatweaksexpanded/module/experience/enchantments/enchantment/DoubleJump.java
+++ src/main/java/insane96mcp/iguanatweaksexpanded/module/experience/enchantments/enchantment/DoubleJump.java
@@ -5,6 +5,7 @@ import insane96mcp.insanelib.world.enchantments.IEnchantmentTooltip;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.EquipmentSlot;
@@ -52,21 +53,25 @@ public class DoubleJump extends Enchantment implements IEnchantmentTooltip {
             return false;
 
         entity.jumpFromGround();
-        RandomSource random = entity.getCommandSenderWorld().getRandom();
-        for (int i = 0; i < 4; i++) {
-            entity.getCommandSenderWorld().addParticle(ParticleTypes.CLOUD, entity.getX() - 0.25f + random.nextFloat() * 0.5f, entity.getY(), entity.getZ() - 0.25f + random.nextFloat() * 0.5f, 0, 0, 0);
-        }
-        entity.playSound(SoundEvents.FIREWORK_ROCKET_LAUNCH, 0.5f, 2f);
         entity.getPersistentData().putInt("double_jumps", entity.getPersistentData().getInt("double_jumps") + 1);
         //Set fallDistance to 0 to prevent falling sound client-side
         entity.fallDistance = 0f;
+        playAnimation(entity);
         return true;
+    }
+
+    public static void playAnimation(Player player) {
+        RandomSource random = player.level().getRandom();
+        if (!player.level().isClientSide) {
+            ((ServerLevel)player.level()).sendParticles(ParticleTypes.CLOUD, player.getX() - 0.25f + random.nextFloat() * 0.5f, player.getY(), player.getZ() - 0.25f + random.nextFloat() * 0.5f, 5, 0, 0, 0, 0);
+        }
+
+        player.playSound(SoundEvents.FIREWORK_ROCKET_LAUNCH, 0.5f, 2f);
     }
 
     public static int getRemainingJumps(Player entity, int lvl) {
         return lvl - entity.getPersistentData().getInt("double_jumps");
     }
-
 
     @Override
     public Component getTooltip(ItemStack stack, int lvl) {
--- src/main/java/insane96mcp/iguanatweaksexpanded/network/message/JumpMidAirMessage.java
+++ src/main/java/insane96mcp/iguanatweaksexpanded/network/message/JumpMidAirMessage.java
@@ -1,5 +1,6 @@
 package insane96mcp.iguanatweaksexpanded.network.message;
 
+import insane96mcp.iguanatweaksexpanded.module.experience.enchantments.enchantment.DoubleJump;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraftforge.network.NetworkDirection;
@@ -22,8 +23,10 @@ public class JumpMidAirMessage {
 
 	public static void handle(final JumpMidAirMessage message, Supplier<NetworkEvent.Context> ctx) {
 		ctx.get().enqueueWork(() -> {
-			if (ctx.get().getSender() != null)
+			if (ctx.get().getSender() != null) {
 				ctx.get().getSender().jumpFromGround();
+				DoubleJump.playAnimation(ctx.get().getSender());
+			}
 		});
 		ctx.get().setPacketHandled(true);
 	}
